652f8e278ee53948620eb8c0dffbddd84c000ec3
Dark Coder <codewithdark90@gmail.com>
2024-07-25 20:53:49 +0500

    Update chatWithWeb.py

--- pages/chatWithWeb.py
+++ pages/chatWithWeb.py
@@ -1,24 +1,29 @@
 import streamlit as st
 from utils.llm import generate_response, generate_prompt
 from utils.common import articles_show, video_show
+from utils.helper import fetch_articles, fetch_videos
+from utils.model import init_db, add_history
 from app import local_css
 
 
-# Function to generate related queries
-async def generate_related_queries(query):
-    prompt = f"Generate 3 related search queries for the following query: '{query}'. Provide only the queries, separated by newlines."
-    response = await generate_response(prompt)
-    return response.strip().split('\n')
-
+# Initialize the database
+init_db()
 local_css("pages/style.css")
 
-st.page_link('app.py', label='Home', icon="🏠")
+st.page_link('app.py', label='Back To Home', icon="🏠")
+st.page_link('pages/history.py', label='See the History', icon="🕒")
+
 query = st.chat_input("Ask me anything...", key="user_query")
 if query:
         st.markdown(f'##### {query}')
         with st.spinner("Researching and analyzing..."):
             prompt = generate_prompt(query)
             response_text = generate_response(prompt)
+
+            articles = fetch_articles(query)
+            video = fetch_videos(query)
+
+            add_history(query, response_text, articles, video)
 
             # Fetch and display articles
             with st.expander('Sources'):
